a1484b0decb718db008c2b756f09dcffd45e440c
Arthur Roux <arthur.roux@epita.fr>
2021-11-28 22:42:34 +0100

    kata drink about / double char

---
+++ index.js
@@ -0,0 +1,13 @@
+//doubleChar("String") ==> "SSttrriinngg"
+
+function doubleChar(str) {
+  var stringTmp = '';
+  for (var i = 0; i < str.length; i++) {
+    //console.log(str[i]);
+    stringTmp += str[i];
+    stringTmp += str[i];
+  }
+  console.log(stringTmp);
+}
+
+doubleChar("string");
